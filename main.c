8126460872a505d3a2c392f60f34825967ef630c
mehrdad-bt <mehrdad.babatabar@gmial.com>
2025-09-03 12:39:05 +0330

    initial commit

---
+++ main.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+
+/*Global Variables and typdefs*/
+
+int head=0; 
+int tail=0;
+
+/*Function Prototype*/
+
+void Write(char, char *);
+int Read(int);
+void PrintBuffer(char *);
+
+// int (*op[3])(int) =
+// {
+//     Write,
+//     Read,
+//     PrintBuffer
+// };
+
+// void Menu(int Choice, int(*op[])(int)){
+
+//     op[Choice]();
+// }
+
+
+int main(){
+
+    char Buffer[3];
+    int Choice = 0;
+    char data;
+
+    while(Choice != 4){
+        printf("Please Enter an Option:\n1=Write\n2=Read\n3=PrintBuffer\n4=Exit\n");
+        scanf(" %d", &Choice);
+        switch(Choice)
+        {
+            case 1:
+            {
+            printf("Please Enter your data:\n");
+            scanf(" %c", &data);
+            Write(data, Buffer);
+            break;
+            }
+            
+            case 3:
+            {
+                PrintBuffer(Buffer);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
+
+void Write(char data, char *Buffer){
+    
+    int flag=0;
+    if((head +1) % 4 == 0)
+    {
+        printf("inside\n");
+        tail = head-1;
+        head = 0;
+        Buffer[head] = data;
+        flag = 1;
+             
+    }
+    if(flag == 1) 
+    {
+        head++;
+    }
+   
+    if(flag == 0 && (head +1) % 4 != 0)
+        {
+            Buffer[head] = data;
+            head++;
+        }
+
+    
+
+        
+    
+    
+    printf("Buffer[ %d ] is  : %c\n", head-1,Buffer[head-1] );
+    
+}
+
+void PrintBuffer(char *Buffer){
+    
+
+
+
+        if(tail ==2)
+        {
+            for(int i = 0; i <= tail; i++)
+            {
+                printf("%c ", Buffer[i]);
+            }
+        }
+        else if(tail ==0)
+        {
+            for(int i = tail; i < head; i++)
+            {
+                printf("%c ", Buffer[i]);
+            }
+        }
+
+
+        
+    
+}
